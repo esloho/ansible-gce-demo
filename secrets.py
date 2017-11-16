38f62d8e6a15ed50a74b35851f2c805967538e7e
esloho <esther@seedtag.com>
2017-11-16 02:26:15 +0100

    Add ansible demo project for gce

---
+++ secrets.py
@@ -0,0 +1,2 @@
+GCE_PARAMS = ('619656904903-compute@developer.gserviceaccount.com', '~/playground/ansible-gce-demo/inventory/ansible_service_account.json')
+GCE_KEYWORD_PARAMS = {'project': 'first-trial-project-186112', 'datacenter': 'europe-west1-b'}
